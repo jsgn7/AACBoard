cf4315adba98568c4f40516aa7e987298f151b13
Javier Sanz <jsgn93@gmail.com>
2026-09-17 13:01:37 +0200

    feat: initial setup with Swift 6 and SwiftData models

---
+++ AACBoard/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  AACBoard
+//
+//  Created by Javier on 17/09/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
